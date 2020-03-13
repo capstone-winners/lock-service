e827e9b30d0adc34c24b010165e11ac52ac6e76d
Thomas Harmon <thomas.alan.harmon@gmail.com>
2020-03-13 16:00:36 -0400

    add init lock script

---
+++ lock.py
@@ -0,0 +1,53 @@
+import time
+# Assumes I2C, a certain type of connection
+import board
+import busio
+
+import qrcode
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+
+# QR Code format:
+#  {
+#   id: int
+#   locked: bool
+#   super: {
+#     deviceId: int,
+#     deviceType: "lock",
+#     status: String,
+#     group: [list of String],
+#     location: String,
+#   }
+#  }
+# }
+
+def getState(sensor):
+    data = {
+        "locked": sensor.pin() == high,
+        "super": {
+            "deviceId": 1,
+            "deviceType": "lock",
+            "status": "ok", # ?
+            "group": ["Trap House"],
+            "location": "Tom's Room"
+        }
+    }
+    return data
+
+state = getState(sensor)
+# TODO: add call to Nikhil's script to make sure qrCode has the right dimensions
+# Then actually display on screen
+qrCode = qrcode.make(state)
+
+
+while True:
+    time.sleep(30)
+    if state != getState(sensor):
+        # The lock status has changed, generate new QR Code
+        state = getState(sensor)
+        # TODO: add call to Nikhil's script to make sure qrCode has the right dimensions
+        # Then actually display on screen
+        qrCode = qrcode.make(state)
+
+
